4050726dfeb5206aee8e2617c906fede97c9b1c8
Ing. Juan Pablo Ruiz <jpruiz@itgcorp.co>
2025-03-28 11:23:05 -0600

    Python Files - Day of class

---
+++ py-e2-01/03-day/taller-03.py
@@ -0,0 +1,9 @@
+"""
+Ejercicio 1:
+Escribe una función llamada validar_edad(edad) 
+que reciba una edad como argumento. 
+Utiliza la instrucción raise para generar una 
+excepción ValueError si la edad es negativa. 
+Si la edad es válida (mayor o igual a 0), 
+la función debe retornar la edad
+"""
